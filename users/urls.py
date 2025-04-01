31b84023d820e377b05a72143f2ba044c0970d99
isaac fitzpatrick <164290144+isaac-fitzpatrick@users.noreply.github.com>
2025-04-02 00:21:07 +1100

    Users Folder Upload

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.user, name="user"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path('register/', views.register, name='register'),
+    path('top-up/', views.top_up, name='top_up')
+]
